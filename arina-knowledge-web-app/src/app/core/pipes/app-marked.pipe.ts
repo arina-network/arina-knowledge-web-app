6df70da2cb278245e65d3e995bb7d4a581778cde
slava <slava@pozotron.com>
2026-06-24 21:37:53 +0300

    Add renderer for local  links in markdown

--- arina-knowledge-web-app/src/app/core/pipes/app-marked.pipe.ts
+++ arina-knowledge-web-app/src/app/core/pipes/app-marked.pipe.ts
@@ -1,14 +1,45 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { inject, Pipe, PipeTransform } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 import * as marked from 'marked';
 
+import { AppParams } from '../constants/app-params';
+import { AppRoutes } from '../constants/app-routes';
+
 @Pipe({
-    name: 'markdown'
+    name: 'markdown',
+    standalone: true
 })
 export class AppMarkdownPipe implements PipeTransform {
+    private route = inject(ActivatedRoute);
+    protected routes = inject(AppRoutes);
+        
     transform(value: string | null | undefined): string {
+        const knowledge = this.routes.knowledge;
+        const owner = this.route.snapshot.paramMap.get(AppParams.Owner);
+        const repository = this.route.snapshot.paramMap.get(AppParams.Repository);
+        const branch = this.route.snapshot.paramMap.get(AppParams.Branch) ?? 'main';
+
         if (!value) {        
             return '';
         }
+
+        // Configure marked to intercept links
+        marked.use({
+            renderer: {
+                link({ href, title, text }) {
+                    let finalHref = href;
+
+                    if (href.startsWith('/')) { // process only local links
+                        finalHref = `${knowledge}/${owner}/${repository}/${branch}/${href}`
+                    }
+
+                    const titleAttr = title ? ` title="${title}"` : '';
+                    return `<a href="${finalHref}"${titleAttr}>${text}</a>`;
+                }
+            }
+        });
+
         // parse the markdown to a raw HTML string and return it
         return marked.parse(value) as string; 
     }
